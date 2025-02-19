18823c96e3a83f95d10aea12fd4395a3774f01f8
Winnie Kelley <w642kelley@gmail.com>
2025-02-19 15:14:42 -0600

    Removed app.js, no longer need it

--- index.js
+++ index.js
@@ -2,21 +2,6 @@ import express from 'express';
 import books from './books.js';
 const app = express();
 
-// // //hardcoded data
-// const books= [
-//   {
-//     "isbn": "05531324567",
-//     "title": "A Court of Wings and Ruin",
-//     "author": "Sarah J. Maas",
-//     "format": "Hardcover"
-//   },
-//   {
-//     "isbn": "05531324511",
-//     "title": "A Court of Silver Flames",
-//     "author": "Sarah J. Maas",
-//     "format": "Hardcover"
-//   }
-// ]
 // route that uses the hardcoded data
 app.get('/books', (req, res) => {
   res.json(books);
@@ -26,11 +11,3 @@ app.get('/books', (req, res) => {
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 })
-
-// const router  = express.Router();
-
-// router.get('/books', (req, res) => {
-//   res.json(books);
-// });
-
-// module.exports = router;
