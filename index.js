b5b61fef9157449907cf60b3d10e7a5e3c54c8be
Winnie Kelley <w642kelley@gmail.com>
2025-02-19 13:25:53 -0600

    Works with external file now

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-const express = require('express');
+import express from 'express';
+import books from './books.js';
 const app = express();
 
-//hardcoded data
-const books= [
-  {
-    "isbn": "05531324567",
-    "title": "A Court of Wings and Ruin",
-    "author": "Sarah J. Maas",
-    "format": "Hardcover"
-  },
-  {
-    "isbn": "05531324511",
-    "title": "A Court of Silver Flames",
-    "author": "Sarah J. Maas",
-    "format": "Hardcover"
-  }
-]
-
+// // //hardcoded data
+// const books= [
+//   {
+//     "isbn": "05531324567",
+//     "title": "A Court of Wings and Ruin",
+//     "author": "Sarah J. Maas",
+//     "format": "Hardcover"
+//   },
+//   {
+//     "isbn": "05531324511",
+//     "title": "A Court of Silver Flames",
+//     "author": "Sarah J. Maas",
+//     "format": "Hardcover"
+//   }
+// ]
 // route that uses the hardcoded data
 app.get('/books', (req, res) => {
   res.json(books);
@@ -26,3 +26,11 @@ app.get('/books', (req, res) => {
 app.listen(3000, () => {
   console.log("Server started on port 3000");
 })
+
+// const router  = express.Router();
+
+// router.get('/books', (req, res) => {
+//   res.json(books);
+// });
+
+// module.exports = router;
